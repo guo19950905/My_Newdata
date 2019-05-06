ba13c097b2f5a49cbd250e8696779334fcaaabb3
guo19950905 <17635370834@163.com>
2019-05-06 17:29:37 +0800

    哈哈哈

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -10,7 +10,7 @@ class TestLogin:
 
     def test_login3(self):
         print("test_login3")
-        assert 1
+        assert 0
     def test_login4(self):
         print("test_login3")
         assert 1
@@ -19,4 +19,4 @@ class TestLogin:
         assert 1
     def test_login6(self):
         print("test_login3")
-        assert 1
+        assert 0
